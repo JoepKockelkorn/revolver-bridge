5d5b947fb6d23c62a27d944bc8aa2fd6e9e82fec
Joep Kockelkorn <joepkockelkorn@gmail.com>
2021-03-06 19:41:17 +0100

    initial commit

---
+++ apps/revolver-bridge/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'revolver-bridge-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'revolver-bridge';
+}
