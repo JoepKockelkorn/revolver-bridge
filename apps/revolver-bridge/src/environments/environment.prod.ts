1364820db61c742e4611cf4aadf927abebc0923e
Joep Kockelkorn <joepkockelkorn@gmail.com>
2021-03-14 14:48:04 +0100

    feat: 🎸 add auth with Google

--- apps/revolver-bridge/src/environments/environment.prod.ts
+++ apps/revolver-bridge/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
   production: true,
+  firebase: {
+    apiKey: 'AIzaSyDm4CjxpmarKjucQdiGFKt50cmeoRKNJys',
+    authDomain: 'revolver-bridge.firebaseapp.com',
+    projectId: 'revolver-bridge',
+    storageBucket: 'revolver-bridge.appspot.com',
+    messagingSenderId: '989441220161',
+    appId: '1:989441220161:web:b0e615ac51362147e130c0',
+  },
 };
